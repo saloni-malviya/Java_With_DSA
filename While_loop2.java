9f0a259f85f4ff0ec28f0fc4d79116b87bc78cf0
Saloni Malviya <100395235+saloni-malviya@users.noreply.github.com>
2024-01-20 18:13:39 +0530

    Add files via upload

---
+++ While_loop2.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+//...........Program to print number from 1 to 10
+package apna_college;
+
+
+public class While_loop2 {
+    public static void main(String[]args)
+    {
+        int i=1;
+        while(i<=10)
+        {
+            System.out.println(i);
+            
+            i++;
+        }
+    }
+}
