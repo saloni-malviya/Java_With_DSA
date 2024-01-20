7f3ea46d99555fa8adbf639573bf57151b78b367
Saloni Malviya <100395235+saloni-malviya@users.noreply.github.com>
2024-01-20 18:12:52 +0530

    Add files via upload

---
+++ While_loop1.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+//................Program to print 'Hello World!!' 100 times using while loop
+package apna_college;
+
+public class While_loop1 {
+    public static void main(String[]args)
+    {
+        int i=1;
+        while(i<=100)
+        {
+            System.out.print("Hello World!!");
+            System.out.print("   ");
+            i++;
+        }
+        
+    }
+    
+}
