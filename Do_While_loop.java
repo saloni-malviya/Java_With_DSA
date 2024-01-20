fc0458ef9a5fb25bf71b1c48b73e0d5fafc94fb5
Saloni Malviya <100395235+saloni-malviya@users.noreply.github.com>
2024-01-20 18:17:06 +0530

    Add files via upload

---
+++ Do_While_loop.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+//...........Program to print "hello world" 10 times using do while loop............
+package apna_college;
+
+public class Do_While_loop {
+    public static void main(String[]args)
+    {
+        int i=1;
+        do{
+          System.out.println("Hello World!!");
+          i++;
+    }while(i<=10);
+}
+}
