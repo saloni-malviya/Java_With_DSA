b17de8ef06de93639366169ad1b9a7c69b76cd75
Saloni Malviya <100395235+saloni-malviya@users.noreply.github.com>
2024-01-20 18:15:12 +0530

    Add files via upload

---
+++ For_loop2.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+//................Program to print square pattern using for loop...............
+package apna_college;
+
+
+public class For_loop2 {
+    public static void main(String[]args)
+    {
+        for(int i=1;i<=4;i++)
+        {
+            System.out.println("* * * *");
+            
+        }
+    }
+}
