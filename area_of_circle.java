25e37e93d0cb051d9889a8cef88b7b04bb298a0a
Saloni Malviya <100395235+saloni-malviya@users.noreply.github.com>
2024-01-22 22:18:08 +0530

    Add files via upload

---
+++ area_of_circle.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package apna_college;
+import java.util.*;
+public class area_of_circle {
+    public static void main(String[]args)
+    {
+        Scanner sc=new Scanner(System.in);
+         System.out.println("Enter radius");
+        float r=sc.nextFloat();
+        float area = 3.14f*r*r;
+         System.out.println("The area of circle is:"+area);
+        
+    }
+}
