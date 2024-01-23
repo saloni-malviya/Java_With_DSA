379b0d09dd8b9bb3299cb788f68d66c41eac6a67
Saloni Malviya <100395235+saloni-malviya@users.noreply.github.com>
2024-01-23 18:29:21 +0530

    Add files via upload

---
+++ Data_types.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package apna_college;
+
+public class Data_types {
+    public static void main(String[]args)
+    {
+        int i=20;
+        float f=12.5f;
+        short s=30;
+        double d=2345556654333333333444444442333333333337.68;
+        long l=33890043333l;
+        char c='a';
+        boolean b=true;
+        byte by=8;
+         System.out.println(i);
+          System.out.println(f);
+           System.out.println(s);
+            System.out.println(d);
+             System.out.println(l);
+              System.out.println(c);
+               System.out.println(b);
+                System.out.println(by);
+    }
+    
+}
