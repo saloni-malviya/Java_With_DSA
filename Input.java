75c8f9e0b433e8c8124ee7b998611116b2c5e396
Saloni Malviya <100395235+saloni-malviya@users.noreply.github.com>
2024-01-23 18:30:55 +0530

    Add files via upload

---
+++ Input.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package apna_college;
+import java.util.*;
+public class Input {
+    public static void main(String[]args)
+    {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter any String");
+        String s=sc.nextLine();
+       // System.out.println("Enter any string with space");
+        //String ss=sc.nextLine();
+        System.out.println("Enter any number");
+        int i=sc.nextInt();
+        System.out.println("Enter any floating number");
+        float num=sc.nextFloat();
+        System.out.println("Enter any long number");
+        long ln=sc.nextLong();
+        
+        System.out.println(s);
+        System.out.println(i);
+        System.out.println(num);
+        System.out.println(ln);
+       
+    }
+    
+}
