62c49a1d9f7bab6c4184885ac8ac944b4deb5a39
Saloni Malviya <100395235+saloni-malviya@users.noreply.github.com>
2024-01-24 19:52:04 +0530

    Add files via upload

---
+++ Pattern4.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+//A
+//BC
+//DEF
+//GHIJ
+package apna_college;
+
+
+public class Pattern4 {
+    public static void main(String[]args)
+    {
+        char ch='A';
+        for(int line=1;line<=4;line++)
+        {
+            for(int chars=1;chars<=line;chars++)
+            {
+                System.out.print(ch);
+                ch++;
+            }
+            System.out.println();
+        }
+    }
+    
+}
