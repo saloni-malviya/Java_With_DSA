13ae672c9bf1ed76bb763fff35d6a14032bb3dc4
Saloni Malviya <100395235+saloni-malviya@users.noreply.github.com>
2024-01-27 21:50:03 +0530

    Add files via upload

---
+++ Fact_Function.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+//..............Program to find factorial of a number..............................
+package apna_college;
+
+public class Fact_Function {
+    public static int fact(int n)
+    {
+        int f=1;
+        for(int i=1;i<=n;i++)
+        {
+            f=f*i;
+        }
+        return f;
+    }
+    public static void main(String[]args)
+    {
+        System.out.println("the Factorial is:"+fact(5));
+    }
+}
