b5b223cc8b6dc9330bb0ab6d655452b95be3b974
Saloni Malviya <100395235+saloni-malviya@users.noreply.github.com>
2024-01-29 22:44:03 +0530

    Add files via upload

---
+++ Largest_2Num.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package apna_college;
+
+
+public class Largest_2Num {
+    public static void main(String[]args)
+    {
+        int a=10;
+        int b=20;
+        if(a>=b)
+        {
+            System.out.println(a+" is greater than "+b);
+        }
+        else
+        {
+            System.out.println(b+" is greater than "+a);
+        }
+    }
+    
+}
