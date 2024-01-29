3d796400e5b3dd7c27961f79c13ce23733eec699
Saloni Malviya <100395235+saloni-malviya@users.noreply.github.com>
2024-01-29 22:44:28 +0530

    Add files via upload

---
+++ Largest_3Num.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package apna_college;
+
+
+public class Largest_3Num {
+    public static void main(String[]args)
+    {
+        int a=5;
+        int b=76;
+        int c=21;
+        if((a>=b) && (a>=c))
+        {
+            System.out.println(a+" is greater");
+        }
+        else if(b>=c)
+        {
+            System.out.println(b+" is greater");
+        }
+        else
+        {
+            System.out.println(c+" is greater");
+        }
+    }
+    
+}
