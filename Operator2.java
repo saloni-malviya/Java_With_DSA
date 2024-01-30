5d65d513ed807032b55576fd80f81481ef91084c
Saloni Malviya <100395235+saloni-malviya@users.noreply.github.com>
2024-01-30 21:32:11 +0530

    Add files via upload

---
+++ Operator2.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+//.......................Relational Operator...........................
+package apna_college;
+
+
+public class Operator2 {
+    public static void main(String[]args)
+    {
+        int a=5;
+        int b=10;
+         System.out.println((a>b));
+         System.out.println((a<b));
+         System.out.println((a>=b));
+         System.out.println((a<=b));
+         System.out.println((a==b));
+         System.out.println((a!=b));
+}
+}
