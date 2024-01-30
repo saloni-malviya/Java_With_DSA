b431c2ce7ab331bbeeb76436045b7b151e761751
Saloni Malviya <100395235+saloni-malviya@users.noreply.github.com>
2024-01-30 21:32:54 +0530

    Add files via upload

---
+++ Operator4.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+//..............Assignment Operator...............
+package apna_college;
+
+
+public class Operator4 {
+    public static void main(String[]args)
+    {
+        int a=10;
+        int b=20;
+        int c=30;
+        int d=40;
+        int e=50;
+        a+=10;
+        System.out.println(a);
+        b-=10;
+        System.out.println(b);
+        c*=10;
+        System.out.println(c);
+        d/=10;
+        System.out.println(d);
+        e%=10;
+        System.out.println(e);
+    }
+    
+}
