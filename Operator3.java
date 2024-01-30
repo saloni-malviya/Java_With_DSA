4e80d52e5f99d7695e4b3acd7ac79e9d8f16a22c
Saloni Malviya <100395235+saloni-malviya@users.noreply.github.com>
2024-01-30 21:32:33 +0530

    Add files via upload

---
+++ Operator3.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+//....................Logical Operator........................
+package apna_college;
+
+
+public class Operator3 {
+    public static void main(String[]args)
+    {
+        int a=5;
+        int b=12;
+        int c=13;
+        int d=7;
+        System.out.println((a>b)&&(c>d));
+        System.out.println((a>b)||(c>d));
+        System.out.println(!(a>b));
+        System.out.println(!(a<b));
+    }
+    
+}
