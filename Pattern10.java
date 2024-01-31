f92667a79d070e07a650f74f23b0795e3d4a89a4
Saloni Malviya <100395235+saloni-malviya@users.noreply.github.com>
2024-01-31 22:34:38 +0530

    Add files via upload

---
+++ Pattern10.java
@@ -0,0 +1,66 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+//........................BUTTERFLY PATTERN................................
+//                         *      *
+//                         **    **
+//                         ***  ***
+//                         ********
+//                         ********
+//                         ***  ***
+//                         **    **
+//                         *      *
+package apna_college;
+
+public class Pattern10 {
+    public static void butterfly(int n)
+    {  
+           //1st half
+        for(int i=1;i<=n;i++)
+        {
+           //Stars
+            for(int j=1;j<=i;j++)
+            {
+                System.out.print("*");
+            }
+            //Spaces
+            for(int j=1;j<=2*(n-i);j++)
+            {
+                System.out.print(" ");
+            }
+            //Stars
+            for(int j=1;j<=i;j++)
+            {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        
+        //2nd half
+        for(int i=n;i>=1;i--)
+        {
+            //stars
+            for(int j=1;j<=i;j++)
+            {
+                System.out.print("*");
+            }
+            //Spaces
+            for(int j=1;j<=2*(n-i);j++)
+            {
+                System.out.print(" ");
+            }
+            //star
+            for(int j=1;j<=i;j++)
+            {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+    public static void main(String[]args)
+    {
+        butterfly(5);
+    }
+    
+}
