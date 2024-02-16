ca6803d3f829e7f93bd6c8a122784f0b2246c5ce
Saloni Malviya <100395235+saloni-malviya@users.noreply.github.com>
2024-02-16 20:12:12 +0530

    Add files via upload

---
+++ Product_of_num.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package apna_college;
+import java.util.*;
+public class Product_of_num {
+    public static void main(String[]args)
+    {
+        Scanner sc=new Scanner(System.in);
+         System.out.println("Enter first number");
+        int num1=sc.nextInt();
+         System.out.println("Enter second number");
+        int num2=sc.nextInt();
+        int product=num1*num2;
+         System.out.println("The product of two numbers is:"+product);
+        
+    }
+}
