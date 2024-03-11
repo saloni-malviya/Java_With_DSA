db08a218a69b50d4c9337947f34b7aef60f4d22a
Saloni Malviya <100395235+saloni-malviya@users.noreply.github.com>
2024-03-11 20:26:59 +0530

    Add files via upload

---
+++ Bubble_sorting_desending.java
@@ -0,0 +1,40 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package apna_college;
+
+
+public class Bubble_sorting_desending {
+    public static void bubble(int a[])
+    {
+        for(int i=0;i<a.length-1;i++)
+        {
+            for(int j=0;j<a.length-1-i;j++)
+            {
+                if(a[j]<a[j+1])
+                {
+                    int temp=a[j];
+                    a[j]=a[j+1];
+                    a[j+1]=temp;
+                }
+            }
+        }
+    }
+    public static void print(int a[])
+    {
+        for(int i=0;i<a.length;i++)
+        {
+            System.out.print(a[i]+" ");
+        }
+        System.out.println(" ");
+    }
+    
+    public static void main(String[]args)
+    {
+        int a[]={2,4,3,1,5};
+        bubble(a);
+        print(a);
+    }
+    
+}
