e38239dfebf95f74cd594ccfe2a96fdf211c6f43
Saloni Malviya <100395235+saloni-malviya@users.noreply.github.com>
2024-03-22 22:25:07 +0530

    Add files via upload

---
+++ Create_String.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package apna_college;
+
+public class Create_String {
+    public static void main(String[]args)
+    {
+        //Three ways to create String
+        //using character array
+        char arr[]={'S','a','l','o','n','i'};
+        //using String literal
+        String s="Saloni";
+        //using new Keyword
+        String s1=new String("Saloni");
+        System.out.println(arr);
+        System.out.println(s1);
+        System.out.println(s);
+    }
+    
+}
