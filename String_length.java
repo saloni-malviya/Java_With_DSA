31da0c27223648cad8e9242a67e669708f2a4da8
Saloni Malviya <100395235+saloni-malviya@users.noreply.github.com>
2024-03-24 20:09:04 +0530

    Add files via upload

---
+++ String_length.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+//we use .length() function to find the length of string
+package apna_college;
+
+public class String_length {
+    public static void main(String[]args)
+    {
+        String s="Saloni Malviya";
+        System.out.println(s.length());
+    }
+    
+}
