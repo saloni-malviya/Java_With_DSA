1825166352ea083714cfadb4fb7344e1bbded21d
Saloni Malviya <100395235+saloni-malviya@users.noreply.github.com>
2024-03-25 23:07:36 +0530

    Add files via upload

---
+++ String_Concatination.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package apna_college;
+
+public class String_Concatination {
+    public static void main(String[]args)
+    {
+        String s="hello";
+        String s1="I am Saloni";
+        //String Concatination
+        String s3=s+" "+s1;
+        System.out.println(s);
+        System.out.println(s1);
+        //output of string concatination
+        System.out.println(s3);
+    }
+}
