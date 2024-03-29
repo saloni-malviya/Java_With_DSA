4c378c03b3ac9f36efe577e5406bd409f3d4a46c
Saloni Malviya <100395235+saloni-malviya@users.noreply.github.com>
2024-03-29 21:34:31 +0530

    Add files via upload

---
+++ Equal_method.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package apna_college;
+
+public class Equal_method {
+    public static void main(String args[])
+    {
+        String s1="Tony";
+        String s2="Tony";
+        String s3=new String("Tony");
+        if(s1.equals(s3))
+        {
+            System.out.println("String are equal");
+        }
+        else
+        {
+            System.out.println("String are not equal");
+        }
+    }
+    
+}
